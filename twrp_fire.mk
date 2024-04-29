058e83cd121956e562204a9d0f75328bbe0c3c0c
YudhoPatrianto <kydh01123@gmail.com>
2024-04-29 17:13:17 +0700

    fire: switch `omni_fire` to `twrp_fire`
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from fire device
 $(call inherit-product, device/xiaomi/fire/device.mk)
 
 PRODUCT_DEVICE := fire
-PRODUCT_NAME := omni_fire
+PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := fire
 PRODUCT_MANUFACTURER := xiaomi
